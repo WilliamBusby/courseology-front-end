34def2741fcd85da481bd180a354adf16a6f4633
William Busby <williambusby1999@gmail.com>
2022-03-07 18:32:16 +0000

    Added main into app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.scss';
 import NavBar from './components/NavBar/NavBar';
+import Main from "./containers/Main/Main";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Main />
     </div>
   );
 }
