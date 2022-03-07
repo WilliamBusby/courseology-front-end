0bc93ba7a747f8c475a66abecd61faba6abce279
William Busby <williambusby1999@gmail.com>
2022-03-07 18:21:38 +0000

    Added basic navbar component

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./NavBar.scss";
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <h2>_courseology</h2>  
+    </div>
+  )
+}
+
+export default NavBar
